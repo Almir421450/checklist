e07d460520f824e07138d9ec2ae7dc617edb7f04
Almir Montovani <almir_rm@hotmail.com>
2022-02-06 15:06:03 -0300

    Iniciando Header, footer, body da nossa aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FooterComponent } from './footer/footer.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CategoryComponent } from './category/category.component';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule,
+    MatListModule,MatDividerModule,MatCardModule, MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/category/category.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../_models/category';
+
+export const CATEGORY_DATA = [
+  {name: 'Educação', guid: 'aaa-bbb-ccc-dddd'},
+  {name: 'Saúde', guid: 'aaa-bbb-ccc-dddd'},
+  {name: 'Trabalho', guid: 'aaa-bbb-ccc-dddd'},
+  {name: 'Outros', guid: 'aaa-bbb-ccc-dddd'}
+];
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+export class CategoryComponent implements OnInit {
+
+  public displayedColuns: string[] = ['id', 'name', 'actions'];
+  public dataSource: Category[] = CATEGORY_DATA;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public editCategory(category: Category){
+    console.log('edit new category clicked');
+  }
+
+  public deleteCategory(category: Category){
+    console.log('delete new category clicked');
+  }
+
+  public creatNewCategory(){
+    console.log('create new category clicked');
+  }
+
+}
